4c3ae5249bc5122352a4558bd5845055b9221d9e
Chanu <kollichanu@gmail.com>
2026-02-03 12:45:06 -0600

    Added .env file dependency

--- src/main/java/com/example/todo_application/TodoApplication.java
+++ src/main/java/com/example/todo_application/TodoApplication.java
@@ -2,11 +2,17 @@ package com.example.todo_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class TodoApplication {
 
+    // Iterate over environment variables and set them as system properties
+    // This is a common method to make them accessible to Spring Boot
+
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(e ->System.setProperty(e.getKey(),e.getValue()));
         SpringApplication.run(TodoApplication.class, args);
     }
 
